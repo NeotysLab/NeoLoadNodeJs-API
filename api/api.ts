63d81bea809848a5df888c7758d218e84a03d415
hrexed <henrik.rexed@neotys.com>
2019-07-08 13:58:14 +0200

    init commit

---
+++ api/api.ts
@@ -0,0 +1,5 @@
+export * from './results.service';
+import { ResultsService } from './results.service';
+export * from './runtime.service';
+import { RuntimeService } from './runtime.service';
+export const APIS = [ResultsService, RuntimeService];
